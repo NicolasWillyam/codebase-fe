fff745bfcd78051c499933c22de85a2da9b8e87f
Dat1205 <hainamlethuy2007@gmail.com>
2025-06-07 23:38:35 +0700

    book service

---
+++ src/application/services/bookingService.ts
@@ -0,0 +1,9 @@
+export class BookingService {
+  async bookTour(userId: string, tourId: string, participants: number) {
+    // Kiểm tra điều kiện và tạo booking
+  }
+
+  async cancelBooking(bookingId: string) {
+    // Hủy đơn đặt tour
+  }
+}
